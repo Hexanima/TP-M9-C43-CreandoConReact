145444bbf1720ead30ff644abd57469bf7c6947e
Hexanima <nicolasfilippelli22@gmail.com>
2023-05-21 19:53:28 -0300

    Practica terminada, lista para corregir

---
+++ src/components/Web/Web.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header/Header";
+import Banner from "../Banner/Banner";
+import Product from "../Product/Product";
+import Footer from "../Footer/Footer";
+function Web() {
+  return (
+    <div className="container">
+      <Header/>
+      <Banner />
+      <Product />
+      <Footer />
+    </div>
+  );
+}
+export default Web;
